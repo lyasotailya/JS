e2a8c9efbf4d89c3daf89687d95312b4ef493f83
illyr <lysota2014@yandex.ru>
2024-07-01 17:43:12 +0300

    Second commit

--- Задание 3/task.js
+++ Задание 3/task.js
@@ -1,16 +1,26 @@
-const clc = document.getElementsByClassName('hole hole_has-mole');
+const hole = document.getElementsByClassName('hole');
 const dead = document.getElementById('dead');
 const lost = document.getElementById('lost');
 
+   
+for (let i = 0; i < 9; i++) {
+    hole[i].onclick = () => {
+        if (hole[i].className.includes( 'hole_has-mole' )) {
+            dead.textContent = Number(dead.textContent) + 1;
 
-dead.textContent = clc.textContent
-console.log(clc)
+            if (dead.textContent == 10) {
+                alert("Вы победили!");
+                dead.textContent = 0;
+                lost.textContent = 0;
+            };
+        } else {
+            lost.textContent = Number(lost.textContent) + 1;
 
-clc.onclick = () => {
-    dead.textContent = clc
-    
-
-    /*setTimeout(() => {
-        
-    }, 100);*/
+            if (lost.textContent == 3) {
+                alert("Вы победили!");
+                dead.textContent = 0;
+                lost.textContent = 0;
+            };
+        }    
+    }
 }
